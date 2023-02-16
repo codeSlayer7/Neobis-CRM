eeee96b08f92919dcbcbd73f13adc0da8a954218
Aizhamal <aijamal.sulaimanova.kk@gmail.com>
2023-02-16 20:22:57 +0600

    fixed size of input

--- src/ui/components/Navbar/Navbar.tsx
+++ src/ui/components/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
       </div>
       <div className="w-1/3 flex items-center justify-between">
         <input className="w-1/3 absolute placeholder:regular placeholder:text-slate-400 block bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Поиск..." type="text" name="search"/>
-        <span className="relative left-[460px]">
+        <span className="relative left-[450px]">
           <img src={search} alt="#" />
         </span>
       </div>
