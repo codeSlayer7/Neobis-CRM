9205f5bb46d019a8543072b63e910e2d5c9a45c8
Aizhamal <aijamal.sulaimanova.kk@gmail.com>
2023-02-16 20:31:56 +0600

    changed styles

--- src/ui/components/Navbar/Navbar.tsx
+++ src/ui/components/Navbar/Navbar.tsx
@@ -12,9 +12,9 @@ function Navbar() {
         <img src={logo} alt="#" />
         <img src={arrow} alt="#" />
       </div>
-      <div className="w-1/3 flex items-center justify-between">
-        <input className="w-1/3 absolute placeholder:regular placeholder:text-slate-400 block bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Поиск..." type="text" name="search"/>
-        <span className="relative left-[450px]">
+      <div className="w-[420px] flex items-center justify-between">
+        <input className="w-[420px] absolute placeholder:regular placeholder:text-slate-400 block bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Поиск..." type="text" name="search"/>
+        <span className="relative left-[350px]">
           <img src={search} alt="#" />
         </span>
       </div>
